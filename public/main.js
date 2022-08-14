2ffb5683ccfd514f293c4309dc34ae2f3c064bb7
jothamardel <ardelmbiplang@gmail.com>
2022-08-14 01:51:47 +0100

    converting image file from colored to grayscale

--- public/main.js
+++ public/main.js
@@ -20,8 +20,8 @@ async function init() {
     const base64 = fileReader.result.replace(
       /^data:image\/(png|jpeg|jpg);base64,/, ''
     );
-    console.log(input.files[0]);
-    console.log(base64);
+    let img_data_url = rustApp.grayscale(base64);
+    document.getElementById('new-img').setAttribute('src', img_data_url);
   }
 
   input.addEventListener('change', () => {
